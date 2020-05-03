9d8d8bdbdc21453fd75b25213fc80353f5c7965a
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-05-03 13:14:53 +0700

    change default routes

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class home extends CI_Controller {
+class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,15 +18,8 @@ class home extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-    public function __construct() {
-		parent::__construct();
-    }
-    
-	public function index() {
+	public function index()
+	{
 		$this->load->view('home_page');
-	}
-
-	public function login() {
-		$this->load->view('login');
 	}
 }
